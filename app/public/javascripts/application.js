e144c88c1e1082074910a9db177282856b1a08a6
João Gradim <joao.gradim@gmail.com>
2010-11-11 21:23:57 +0000

    Great improvements on video loading and event binding

--- app/public/javascripts/application.js
+++ app/public/javascripts/application.js
@@ -1,32 +1,6 @@
 $(document).ready(function() {
 
-  // play / pause for both players
-  $("button.play").click(function() {
-    var fn_play = "playVideo"+$(this).parent().data('player-id');
-    var fn_pause = "pauseVideo"+$(this).parent().data('player-id');
-    var playing = $(this).data('playing');
-    
-    if(playing) {
-      window[fn_pause]();
-    }
-    else {
-      window[fn_play]();
-    }
-    $(this).data('playing', !playing);
-  });
+  create_player("#player-left", 1);
+  //create_player("#player-right", 2);
   
-  // mute / unmute
-  $("button.mute").click(function(){
-    var fn_mute = "muteVideo"+$(this).parent().data('player-id');
-    var fn_unmute = "unMuteVideo"+$(this).parent().data('player-id');
-    var muted = $(this).data('muted');
-    
-    if(muted) {
-      window[fn_unmute](); 
-    }
-    else {
-      window[fn_mute]();
-    }
-    $(this).data('muted', !muted);
-  });
 });
